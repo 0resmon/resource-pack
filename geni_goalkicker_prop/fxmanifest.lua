987b3da18bcebfdc967d6593e035310e99f9eb33
Emir Kibar <62299912+kibradev@users.noreply.github.com>
2024-06-11 10:29:02 +0300

    Add files via upload

---
+++ geni_goalkicker_prop/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+author 'Quadria'
+description 'Goal Kicker'
+this_is_a_map 'yes'
+data_file 'DLC_ITYP_REQUEST' 'stream/quadria_soccergate.ytyp'
+lua54 'yes'
+escrow_ignore {
+    'stream/quadria_soccergate.ydr',
+    'stream/quadria_soccergate.ytyp',
+    'stream/quadria_soccergate_ball.ydr',
+    'stream/quadria_soccergate_ball_stick.ydr'
+}
